018ce147b435bc16910dcdce0787bdcaab1c51a7
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-02 23:04:15 +0530

    Update C++Sol.cpp

--- Series/Padovan Series/C++Sol.cpp
+++ Series/Padovan Series/C++Sol.cpp
@@ -1,24 +1,25 @@
-int pad(int n) 
+#include <stdio.h> 
+#include<iostream>
+using namespace std;
+int lucas(int n) 
 { 
 
-    int prevprev = 1, prev = 1, curr = 1, next = 1 , rev=1; 
+    if (n == 0) 
+        return 2; 
+    if (n == 1) 
+        return 1; 
   
-    for (int i=3; i<=n; i++) 
-    { 
-        next = prevprev + prev; 
-        prevprev = rev; 
-        rev = curr; 
-        curr = next; 
-    } 
-  
-    return next; 
+    // recurrence relation 
+    return lucas(n - 1) +  
+           lucas(n - 2); 
 } 
   
-
+// Driver Code 
 int main() 
 { 
-    cin>>"Enter the position ";
-  cin>>n;
-    cout << pad(n); 
+    int n ;
+    cout<<"enter the position of the element of the series ";
+    cin>>n;
+    printf("%d", lucas(n)); 
     return 0; 
 } 
